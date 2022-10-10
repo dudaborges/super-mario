be2438bd4aa6f3d652b1d1b673dfcf1146e61913
dudaborges <duda.pborges92@gmail.com>
2022-10-10 18:39:47 -0300

    finished: jump animation script

--- js/script.js
+++ js/script.js
@@ -3,9 +3,15 @@ const mario = document.querySelector('.mario')
 const jump = () => {
     mario.classList.add('jump')
     // para adicionar a class jump no mario. Mesma coisa que escrever no class do mario assim: class="mario jump"
+
+    // para remover o "jump" do mario. É preciso remover, porque se não a animação acontece apenas uma vez. E também antes de remover é preciso respeitar o tempo de animação do pulo que é de 500ms.
+    setTimeout(() => {
+        mario.classList.remove('jump')
+    }, 500)
+
 }
 
-// para remover o "jump" do mario. É preciso remover, porque se não a animação acontece apenas uma vez. E também antes de remover é preciso respeitar o tempo de animação do pulo que é de 500ms.
+
 
 document.addEventListener('keydown', jump)
 // o evento keydown é quando pressionar a tecla
